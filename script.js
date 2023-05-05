2d8e34c09cead2d573750533fdbfee8c2fe5f2ec
suzyhan <129558186+suzyhan@users.noreply.github.com>
2023-05-04 18:06:43 -0600

    Add variables for user input and character types

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 // Assignment code here
 
+// Create variables for user input and character types
+var userInput = [];
+var characterLength = 8;
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -13,5 +23,11 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Add and define function generatePassword based on series of prompts for password criteria
+function generatePassword() {
+  // Will log statement in console to confirm button is clicked and working
+  console.log("You clicked the button!");
+}
+
+// Prompt the user for the password criteria
+// Password length of 8-128 characters
